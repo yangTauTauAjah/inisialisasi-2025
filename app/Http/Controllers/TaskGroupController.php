bc37929eb02481256b5b9e885d195a96f7089499
Habib Anwash <tesakun6464@gmail.com>
2025-06-29 09:20:54 +0700

    getAll

--- app/Http/Controllers/TaskGroupController.php
+++ app/Http/Controllers/TaskGroupController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\TaskGroup;
 use Illuminate\Http\Request;
+use function Laravel\Prompts\alert;
 
 class TaskGroupController extends Controller
 {
@@ -12,7 +13,7 @@ class TaskGroupController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(TaskGroup::all());
     }
 
     /**
@@ -55,16 +56,38 @@ class TaskGroupController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(  $task_group)
+    public function update(Request $request, $id)
     {
-        //
+        $taskGroup = TaskGroup::find($id);
+        if ($taskGroup) {
+            $taskGroup->update([
+                'task_group_name' => $request->task_group_name,
+            ]);
+            return redirect()->back()->with('success', 'Tugas berhasil diedit');
+        }
+        alert('Tugas tidak ditemukan');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy( $task_group)
+    public function destroy( $id)
     {
-        //
+        $taskGroup = TaskGroup::find($id);
+        if ($taskGroup) {
+            $taskGroup->delete();
+            // return response()->isOk();
+            return redirect(url()->current());
+            // return view('admin.tugas', compact($taskGroup::all()))->with('success', 'Tugas berhasil dihapus');
+            // return response()->json([
+            //     'status' => 'success',
+            //     'message' => 'Tugas berhasil dihapus'
+            // ]);
+        }
+        alert('Tugas tidak ditemukan');
+        // return response()->json([
+        //     'status' => 'error',
+        //     'message' => 'Tugas tidak ditemukan'
+        // ], 404);
     }
 }
